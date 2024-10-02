2a0f49084081b5dcc5779a02d7780db5fccd1afc
rana saad <bitf21m041@pucit.edu.pk>
2024-10-02 18:41:40 +0500

    changes for a reason

--- src/components/survey/results/SurveyResultClientPage.tsx
+++ src/components/survey/results/SurveyResultClientPage.tsx
@@ -6,6 +6,7 @@ import { JP } from "@/assets/images";
 const SurveyResultClientPage = () => {
   return (
     <>
+    
       <section className="min-h-screen h-full bg-surveyResult bg-no-repeat bg-bottom bg-cover">
         <div className="max-w-7xl h-full mx-auto sm:flex sm:flex-col sm:gap-8 justify-between ~px-4/12 pb-4 ~/md:~pt-32/40 lg:pt-22">
           <HeadCard
