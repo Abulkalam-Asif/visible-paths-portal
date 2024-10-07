67be33f389e0e0c29e6b650148c0fca7081be064
rana saad <bitf21m041@pucit.edu.pk>
2024-10-07 23:43:45 +0500

    fixed again x3

--- src/components/finance/loan-lease/LoanLeaseClientPage.tsx
+++ src/components/finance/loan-lease/LoanLeaseClientPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import LoanLeaseLayout from "./LoanLeaseLayout";
 import LoanLeaseLayoutInnerComp from "./LoanLeaseLayoutInnerComp";
 
@@ -6,9 +6,11 @@ const LoanLeaseClientPage = () => {
   return (
     <section className="min-h-screen h-auto bg-survey bg-no-repeat bg-top bg-cover">
       <div className="max-w-7xl mx-auto ~px-4/12 pb-4 ~/md:~pt-36/40 lg:pt-36">
-        <LoanLeaseLayout>
-          <LoanLeaseLayoutInnerComp />
-        </LoanLeaseLayout>
+        <Suspense fallback={<div>Loading survey...</div>}>
+          <LoanLeaseLayout>
+            <LoanLeaseLayoutInnerComp />
+          </LoanLeaseLayout>
+        </Suspense>
       </div>
     </section>
   );
