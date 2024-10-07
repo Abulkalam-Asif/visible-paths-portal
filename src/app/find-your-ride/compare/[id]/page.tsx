3a9bb8c927d0a839c016dddb91c769f42c090a25
Muhammad Abul Kalam Asif <abulkalamasif1129@gmail.com>
2024-10-08 00:34:05 +0500

    Refactor CompareId component to remove unused code and comments

--- src/app/find-your-ride/compare/[id]/page.tsx
+++ src/app/find-your-ride/compare/[id]/page.tsx
@@ -2,20 +2,23 @@ import CompareClientPage from "@/components/findYourRide/compare/CompareClientPa
 import carsData from "@/../public/temp/compare.json";
 import React from "react";
 
-type CompareIdProps = {
-  params: {
-    id: string;
+// Get ID from here
+// type CompareIdProps = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+const CompareId = () =>
+  // { params: { id } }: CompareIdProps
+  {
+    // Fetch data for the car with the given id
+
+    return (
+      <>
+        <CompareClientPage cars={carsData} />
+      </>
+    );
   };
-};
-
-const CompareId = ({ params: { id } }: CompareIdProps) => {
-  // Fetch data for the car with the given id
-
-  return (
-    <>
-      <CompareClientPage cars={carsData} />
-    </>
-  );
-};
 
 export default CompareId;
