3090320ca14f1aab9c4b636172997ac061426e8b
Muhammad Abul Kalam Asif <abulkalamasif1129@gmail.com>
2024-10-12 22:34:03 +0500

    compare page done

--- src/components/findYourRide/compare/CompareCarCard.tsx
+++ src/components/findYourRide/compare/CompareCarCard.tsx
@@ -24,14 +24,14 @@ const CompareCarCard = ({
 }: CompareCarCardProps) => {
   return (
     <>
-      <div className="w-full bg-[#737475]/30 backdrop-blur-md border-[3px] border-white rounded-tr-[3.5rem] rounded-b-lg px-6 pt-5 text-white flex flex-col">
+      <div className="w-full bg-[#F6F4F1] rounded-tr-[3.5rem] rounded-b-lg px-6 pt-5 flex flex-col lg:~lg:~px-3/6 lg:~lg:~pt-2/5 lg:~lg:~rounded-tr-[2rem]/[3.5rem]">
         <div className="relative z-0 w-full flex-1 flex flex-col">
           <div className="w-[calc(100%+4px)] translate-x-[-2px] relative z-[1]">
             <Image
               src={compareCarBgImg}
               alt="car"
               width={300}
-              height={251.89}
+              height={250}
               className="w-full h-auto"
             />
             <Image
@@ -41,30 +41,30 @@ const CompareCarCard = ({
               height={300}
               className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 h-auto"
             />
-            <span className="absolute top-4 left-0 text-lg font-bold bg-black/20 px-3 py-1 border border-white rounded-l rounded-r-3xl">
+            <span className="absolute top-4 left-0 text-lg font-bold bg-[#F4F3F0] px-3 py-1 border border-black/60 rounded-l rounded-r-3xl lg:~lg:~text-sm/2xl lg:~lg:~top-2/4">
               {name}
             </span>
           </div>
-          <div className="flex-1 relative pt-24 pb-16 rounded-b-md -translate-y-16 border border-white flex flex-col items-center gap-2 bg-black/20 backdrop-blur-[2px] lg:pt-20">
-            <h2 className="text-2xl font-bold bg-black/20 border border-white backdrop-blur-[2px] px-3 py-1 rounded">
+          <div className="flex-1 relative pt-24 pb-16 rounded-b-md -translate-y-16 border border-[#C7C7C7] flex flex-col items-center gap-2 bg-white lg:~lg:~pt-16/20">
+            <h2 className="text-2xl font-bold bg-white border border-[#C7C7C7] px-[0.3em] py-[0.1em] rounded lg:~lg:~text-base/3xl">
               ${price}
             </h2>
-            <div className="flex items-center gap-2 text-lg font-bold bg-black/20 border border-white backdrop-blur-[2px] px-3 py-1 rounded">
+            <div className="flex items-center gap-2 text-lg font-bold bg-white border border-[#C7C7C7] px-[0.4em] py-[0.1em] rounded lg:~lg:~text-sm/2xl lg:~lg:~gap-2/4">
               <Image
                 src={prosIcon}
                 alt="pros"
                 width={24}
                 height={24}
-                className="w-5 h-auto"
+                className="w-5 h-auto lg:~lg:~w-3/6"
               />
               <h3>Pros</h3>
             </div>
-            <ul className="list-disc w-3/4 text-sm space-y-1 font-bold mt-1 lg:text-base">
+            <ul className="list-disc w-3/4 text-sm space-y-1 font-bold mt-1 lg:~lg:~text-xxs/base lg:~lg:~space-y-0/1.5">
               {pros.map((pro, index) => (
                 <li key={index}>{pro}</li>
               ))}
             </ul>
-            <div className="mt-2 flex items-center gap-2 text-lg font-bold bg-black/20 border border-white backdrop-blur-[2px] px-3 py-1 rounded">
+            <div className="mt-2 flex items-center gap-2 text-lg font-bold bg-white border border-[#C7C7C7] px-3 py-1 rounded">
               <Image
                 src={prosIcon}
                 alt="cons"
@@ -74,7 +74,7 @@ const CompareCarCard = ({
               />
               <h3>Cons</h3>
             </div>
-            <ul className="list-disc w-3/4 text-sm space-y-1 font-bold mt-1 lg:text-base">
+            <ul className="list-disc w-3/4 text-sm space-y-1 font-bold mt-1 lg:~lg:~text-xxs/base lg:~lg:~space-y-0/1.5">
               {cons.map((con, index) => (
                 <li key={index}>{con}</li>
               ))}
@@ -88,7 +88,7 @@ const CompareCarCard = ({
         </div>
       </div>
       {index !== 2 && (
-        <div className="hidden lg:flex self-center text-white text-3xl items-center justify-center font-black text-center bg-black w-16 h-16 min-w-16 min-h-16 rounded-full">
+        <div className="hidden lg:flex self-center text-white ~lg:~text-base/2xl items-center justify-center font-black text-center bg-black ~lg:~w-10/16 ~lg:~h-10/16 ~lg:~min-w-10/16 ~lg:~min-h-10/16 rounded-full">
           <span>VS</span>
         </div>
       )}
--- src/components/findYourRide/compare/CompareClientPage.tsx
+++ src/components/findYourRide/compare/CompareClientPage.tsx
@@ -15,8 +15,8 @@ type CompareClientPageProps = {
 const CompareClientPage = ({ cars }: CompareClientPageProps) => {
   return (
     <>
-      <section className="min-h-screen h-auto bg-survey bg-no-repeat bg-top bg-cover">
-        <div className="max-w-6xl mx-auto ~px-4/12 pb-4 ~/md:~pt-28/36 lg:~pt-24/32">
+      <section className="min-h-screen h-auto bg-main bg-no-repeat bg-top bg-cover">
+        <div className="~lg:~max-w-3xl/7xl mx-auto ~px-4/12 pb-4 ~/md:~pt-32/40 lg:~lg:~pt-28/56">
           <div className="grid grid-cols-1 gap-y-8 gap-x-4 sm:grid-cols-2 lg:flex lg:items-stretch lg:gap-2">
             {cars.map((car, index) => (
               <CompareCarCard key={index} index={index} {...car} />
