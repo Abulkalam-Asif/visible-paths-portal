8603d7b68dbd2f2493b35f634c5c1c58a141fe07
Muhammad Abul Kalam Asif <abulkalamasif1129@gmail.com>
2024-10-13 14:22:16 +0500

    homepage done

---
+++ src/components/general/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-full absolute -translate-y-full">
+        <p className="w-full text-center font-bold text-sm pb-2 lg:~lg:~text-sm/lg lg:~lg:~pb-2/4">
+          &copy; 2024 VisiblePaths, Inc. All Rights Reserved.
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/home/HomeClientPage.tsx
+++ src/components/home/HomeClientPage.tsx
@@ -1,20 +1,39 @@
 import React from "react";
 import HomeLink from "./HomeLink";
-import { circleBlackImg, circleOrangeImg, circleRedImg } from "@/assets/images";
+import {
+  circleBlackImg,
+  circleOrangeImg,
+  circleRedImg,
+  homeCarImg,
+} from "@/assets/images";
+import Image from "next/image";
 
 const HomeClientPage = () => {
   return (
     <>
       <section className="min-h-screen h-auto bg-main bg-no-repeat bg-top bg-cover">
-        <div className="min-h-screen ~lg:~max-w-3xl/7xl mx-auto ~px-4/8 pb-4 flex items-center justify-center ~/md:~pt-36/40 lg:pt-0">
+        <div className="~lg:~max-w-3xl/7xl mx-auto ~px-4/8 pb-4 ~/md:~pt-36/40 lg:~lg:~pt-32/64 lg:~lg:~pb-6/10">
           <div className="w-full flex flex-col items-center gap-4 sm:~sm:~gap-4/20 sm:flex-row sm:justify-evenly">
             <HomeLink
               text="Explore Finance"
               href="/"
               circleImg={circleBlackImg}
             />
-            <HomeLink text="Pick Ride" href="/" circleImg={circleOrangeImg} />
+            <HomeLink
+              text="Pick Ride"
+              href="/find-your-ride"
+              circleImg={circleOrangeImg}
+            />
             <HomeLink text="Compare Rates" href="/" circleImg={circleRedImg} />
+          </div>
+          <div className="mt-4 lg:~lg:~mt-6/12">
+            <Image
+              src={homeCarImg}
+              alt="Car"
+              width={500}
+              height={500}
+              className="w-3/4 h-auto mx-auto"
+            />
           </div>
         </div>
       </section>
