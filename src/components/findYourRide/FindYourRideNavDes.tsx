c7b8050019d6f208e6274989fd5bbdf749ff459c
Muhammad Abul Kalam Asif <abulkalamasif1129@gmail.com>
2024-10-13 15:38:26 +0500

    Refactor FindYourRideNavDes component to remove unused imports and fix typo in import statement

--- src/components/findYourRide/FindYourRideNavDes.tsx
+++ src/components/findYourRide/FindYourRideNavDes.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { FaChevronRight } from "react-icons/fa";
 import { FaArrowRightArrowLeft, FaMagnifyingGlass } from "react-icons/fa6";
 import FindYourRideNavMenu from "./FindYourRideNavMenu";
