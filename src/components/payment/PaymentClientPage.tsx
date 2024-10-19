ccec4c4154bba2de3967d57fe8261fecd4d3dbc6
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2024-10-18 21:57:43 -0700

    Refactor PaymentClientPage component to add suspense fallback

--- src/components/payment/PaymentClientPage.tsx
+++ src/components/payment/PaymentClientPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 import PaymentScreen1 from "./PaymentScreen1";
 import PaymentScreen2 from "./PaymentScreen2";
 import PaymentScreen3 from "./PaymentScreen3";
@@ -8,14 +8,15 @@ import PaymentScreen3 from "./PaymentScreen3";
 const PaymentClientPage = () => {
   const searchParams = useSearchParams();
   const currentScreen = searchParams.get("screen") || "1";
+
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <section className="min-h-screen h-auto bg-main bg-no-repeat bg-top bg-cover">
         {currentScreen === "1" && <PaymentScreen1 />}
         {currentScreen === "2" && <PaymentScreen2 />}
         {currentScreen === "3" && <PaymentScreen3 />}
       </section>
-    </>
+    </Suspense>
   );
 };
 
