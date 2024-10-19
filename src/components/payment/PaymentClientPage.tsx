3dff9e5a12968143f694ebb99b15c648f144f3ea
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2024-10-18 22:00:31 -0700

    Suspense modified

--- src/components/payment/PaymentClientPage.tsx
+++ src/components/payment/PaymentClientPage.tsx
@@ -5,17 +5,22 @@ import PaymentScreen1 from "./PaymentScreen1";
 import PaymentScreen2 from "./PaymentScreen2";
 import PaymentScreen3 from "./PaymentScreen3";
 
-const PaymentClientPage = () => {
+const PaymentClientPageContent = () => {
   const searchParams = useSearchParams();
   const currentScreen = searchParams.get("screen") || "1";
+  return (
+    <section className="min-h-screen h-auto bg-main bg-no-repeat bg-top bg-cover">
+      {currentScreen === "1" && <PaymentScreen1 />}
+      {currentScreen === "2" && <PaymentScreen2 />}
+      {currentScreen === "3" && <PaymentScreen3 />}
+    </section>
+  );
+};
 
+const PaymentClientPage = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <section className="min-h-screen h-auto bg-main bg-no-repeat bg-top bg-cover">
-        {currentScreen === "1" && <PaymentScreen1 />}
-        {currentScreen === "2" && <PaymentScreen2 />}
-        {currentScreen === "3" && <PaymentScreen3 />}
-      </section>
+      <PaymentClientPageContent />
     </Suspense>
   );
 };
